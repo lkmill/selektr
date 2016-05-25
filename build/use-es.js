ee29d0ef6f128f6dffcc803c57799eb8a889c024
Linus Miller <lohfu@lohfu.io>
2016-05-25 15:28:01 +0200

    More restrictive regex for importing dollr/es

--- build/use-es.js
+++ build/use-es.js
@@ -4,7 +4,7 @@ module.exports = function () {
       ImportDeclaration(path) {
         const source = path.node.source;
         source.value = source.value.replace(/^lodash($|\/)/, 'lodash-es$1');
-        source.value = source.value.replace(/^dollr($|\/)/, 'dollr/es$1');
+        source.value = source.value.replace(/^dollr(?!\/es)($|\/)/, 'dollr/es$1');
       }
     }
   };
